2543269e1834e62bcdd8783b9783446dba308ef5
S <s>
2013-10-27 21:36:21 -0700

    {"type":"TI", "runs":"1"}

--- Circles.java
+++ Circles.java
@@ -1,17 +1,32 @@
 
+import java.awt.Color;
+
 import acm.program.ConsoleProgram;
 import acm.program.GraphicsProgram;
 import acm.util.*;
 import acm.graphics.*;
 
-public class Circles extends ConsoleProgram {
+public class Circles extends GraphicsProgram {
 
 	public void run() {
 		
-		for (int i = 0; i < 10; i++){
-			println(rgen.nextInt(0, 200));
-		}
+		// Variables
+		int x = rgen.nextInt(0, getWidth());
+		int y = rgen.nextInt(0, getHeight());
+		int r = rgen.nextInt(5, 50);
+				
+		add(randomCircle(x, y, r, Color.RED));
+
 
 	}
+	
+	
+	private GOval randomCircle(int x, int y, int r, Color color){ 
+		GOval circle = new GOval(x-r,y-r,2*r,2*r);
+		circle.setFilled(true);
+		circle.setColor(color);
+		return circle;
+	}
+	
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 }
