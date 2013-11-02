39e6ce9224399b487113ae57ecdf7e3c3a3e8ec8
S <s>
2013-11-02 15:32:25 -0700

    {"type":"TI", "runs":"1"}

--- MouseTracker.java
+++ MouseTracker.java
@@ -11,23 +11,24 @@ import java.awt.event.*;
 public class MouseTracker extends GraphicsProgram { 
  
  public void run() { 
- label = new GLabel(""); 
- label.setFont("Times New Roman-36"); 
- add(label, 50, 50); 
  
- circle = new GOval(10,10, 30, 30);
- circle.setFilled(true);
- add(circle);
+	 label = new GLabel(""); 
+	 label.setFont("Times New Roman-36"); 
+	 add(label, 50, 50); 
+ 
+	 circle = new GOval(10,10, 30, 30);
+	 circle.setFilled(true);
+	 add(circle);
  
  
- // Must call this method to be able to get mouse events 
- addMouseListeners(); 
+	 // Must call this method to be able to get mouse events 
+	 addMouseListeners(); 
  } 
  
  // This method is called everytime user moves mouse 
  public void mouseMoved(MouseEvent e) { 
- label.setLabel("Mouse: (" + e.getX() + ", " + e.getY() + ")"); 
- label.setLocation(e.getX()-label.getWidth()/2, e.getY());
+	 label.setLabel("Mouse: (" + e.getX() + ", " + e.getY() + ")"); 
+	 label.setLocation(e.getX()-label.getWidth()/2, e.getY());
  } 
  
  public void circleMoved(MouseEvent e){
