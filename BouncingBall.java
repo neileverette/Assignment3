06d3706429bc233105054b97cc11d9bd8fde1593
S <s>
2013-11-03 21:23:08 -0800

    {"type":"TI", "runs":"1"}

--- BouncingBall.java
+++ BouncingBall.java
@@ -6,50 +6,41 @@ import acm.program.GraphicsProgram;
 
 public class BouncingBall extends GraphicsProgram{
 	
-	private static final int BALL_RADIUS = 20;
-	private static final double VELOCITY = 33;
+	private static final int DIAM_BALL = 30;
 	private static final double GRAVITY = 3;
 	private static final double DELAY = 50;
-	private static final double X_START = BALL_RADIUS;
-	private static final double Y_START = 10;
+	private static final double X_START = DIAM_BALL;
+	private static final double Y_START = 100;
 	private static final double X_VEL=5;
 	private static final double BOUNCE_REDUCE = 0.9;
 	
 	private double xVel = X_VEL;
 	private double yVel = 0.0;
 	
-	private GOval bouncingBall;
+	private GOval ball;
 	
 	public void run(){
-		createBall(X_START,Y_START);
+		setup();
 		
-		while(bouncingBall.getX() < getWidth()){
+		while(ball.getX() < getWidth()){
 			moveBall();
 			checkForCollision();
-			pause(DELAY);
-			
+			pause(DELAY);			
 		}	
 	}
 	
-	private void createBall(double x, double y){
-		bouncingBall = new GOval(x,y, 2*BALL_RADIUS,2*BALL_RADIUS);
-		bouncingBall.setFilled(true);
-		bouncingBall.setColor(Color.BLACK);
-		add(bouncingBall);
+	private void setup(){
+		ball = new GOval(X_START,Y_START, DIAM_BALL, DIAM_BALL);
+		ball.setFilled(true);
+		ball.setColor(Color.BLACK);
+		add(ball);
 	}
 
 	private void moveBall(){
-		yVel += GRAVITY;
-		bouncingBall.move(X_VEL, yVel);
+
 	}
 	
 	private void checkForCollision(){
-		if (bouncingBall.getY() > getHeight() - 2*BALL_RADIUS);{
-			yVel = -yVel*BOUNCE_REDUCE;
-			
-			double diff = bouncingBall.getY() -(getHeight() - 2*BALL_RADIUS);
-			bouncingBall.move(0,-2 * diff);
-		}
 		
 	}
 }
