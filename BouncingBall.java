d098116eb183255957f4bf7cf13e49d1c2cbea84
S <s>
2013-11-03 18:06:42 -0800

    {"type":"TI", "runs":"1"}

--- BouncingBall.java
+++ BouncingBall.java
@@ -7,7 +7,7 @@ import acm.program.GraphicsProgram;
 public class BouncingBall extends GraphicsProgram{
 	
 	private static final int BALL_RADIUS = 20;
-	private static final double VELOCITY = 100;
+	private static final double VELOCITY = 50;
 	
 	private GOval bouncingBall;
 	
