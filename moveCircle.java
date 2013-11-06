bbc66fe0470c73432eecde89657e30688a60589f
S <s>
2013-11-06 08:47:40 -0800

    {"type":"TI", "runs":"0"}

--- moveCircle.java
+++ moveCircle.java
@@ -1,4 +1,19 @@
+import acm.graphics.*;
+import acm.program.*;
 
-public class moveCircle {
+
+public class moveCircle extends GraphicsProgram{
+	
+	// Constants
+	private static final double DIAM = 30;
+	private static final double X_DIMIN = 0 + DIAM/2;
+	private static final double Y_DIMIN = 0 + DIAM/2;
+	
+	// Instance variable
+	ball = new ball(double X_DIMIN,double Y_DIMIN, DIAM, DIAM);
+	
+	public void run(){
+		
+	}
 
 }
