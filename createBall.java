e9b38c9d6d6739dc00d4e1532fedd96993e817bc
S <s>
2013-11-10 19:38:36 -0800

    {"type":"TI", "runs":"0"}

--- createBall.java
+++ createBall.java
@@ -1,4 +1,17 @@
 
 public class createBall {
+	
+	public createBall(double x, double y, double xPos, double yPos){
+		this.x = x;
+		this.y = y;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		
+	}
+	
+	private double x;
+	private double y;
+	private double xPos;
+	private double yPos;
 
 }
