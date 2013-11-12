c44748731e8d32e14d03e1f55d25889277fc3e45
S <s>
2013-11-12 06:50:18 -0800

    {"type":"TI", "runs":"1"}

--- moveCircle.java
+++ moveCircle.java
@@ -15,7 +15,7 @@ public class moveCircle extends GraphicsProgram{
 	private GOval ball;
 	
 	public void run(){
-		createBall(X_DIMIN, Y_DIMIN, DIAM,DIAM);
+		createBall(X_DIMIN, Y_DIMIN, DIAM);
 		
 		
 	}
