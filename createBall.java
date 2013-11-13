55608b4d2c0271fc1eb5837537f3591fc5a6f9a0
S <s>
2013-11-12 17:34:40 -0800

    {"type":"TI", "runs":"0"}

--- createBall.java
+++ createBall.java
@@ -6,20 +6,5 @@ import acm.graphics.*;
 
 public class createBall {
 
-	private double x;
-	private double y;
-	private double d;
-	private GOval ball;	
-
-
-	public GOval createBall(double x, double y, double d){
-		this.x = x;
-		this.y = y;
-		this.d = d;
-		
-		ball = new GOval(x,y,d,d);
-		ball.setFilled(true);
-		ball.setFillColor(Color.BLACK);
-		return ball;
-	}
 }
+
