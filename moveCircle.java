c186201ffa82efb87935a2dbe6a8a78ad93e5673
S <s>
2013-11-13 22:42:06 -0800

    {"type":"TI", "runs":"0"}

--- moveCircle.java
+++ moveCircle.java
@@ -20,11 +20,11 @@ public class moveCircle extends GraphicsProgram{
 		
 	}
 	
+	// This method creates the ball
 	private void createBall(){
-		ball = new GOval(100, 100, 30, 30);
+		ball = new GOval (100, 100, 30, 30);
 		ball.setFilled(true);
 		ball.setColor(Color.BLACK);
-		add(ball);	
+		add(ball);
 	}
-
 }
