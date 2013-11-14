f21df8f7ca23de0770e92f14b132b11b683138bb
S <s>
2013-11-13 22:43:32 -0800

    {"type":"TI", "runs":"1"}

--- moveCircle.java
+++ moveCircle.java
@@ -8,8 +8,8 @@ public class moveCircle extends GraphicsProgram{
 	
 	// Constants
 	private static final double DIAM = 30;
-	private static final double X_DIMIN = 0 + DIAM/2;
-	private static final double Y_DIMIN = 0 + DIAM/2;
+	private static final double X_DIMIN = DIAM/2;
+	private static final double Y_DIMIN = DIAM/2;
 	
 	private GOval ball;
 	
@@ -22,7 +22,7 @@ public class moveCircle extends GraphicsProgram{
 	
 	// This method creates the ball
 	private void createBall(){
-		ball = new GOval (100, 100, DIAM, DIAM);
+		ball = new GOval (X_DIMIN, Y_DIMIN, DIAM, DIAM);
 		ball.setFilled(true);
 		ball.setColor(Color.BLACK);
 		add(ball);
