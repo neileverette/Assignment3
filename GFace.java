f5cead3a04418ea6528bcd38a7364655ae7fc535
S <s>
2013-11-16 10:29:28 -0800

    {"type":"TI", "runs":"0"}

--- GFace.java
+++ GFace.java
@@ -1,4 +1,15 @@
 
-public class GFace {
+import acm.graphics.*; 
+ 
+public class GFace extends GCompound { 
+ 
 
-}
+ public GFace(double width, double height) { 
+	 head = new GOval(width, height); 
+	 add(head, 0, 0); 
+ }
+ 
+ /* Private instance variables */ 
+ private GOval head; 
+
+} 
