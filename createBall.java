dc2f24da5c2d323a43e45b0c70067498b2d22577
S <s>
2013-11-16 14:00:41 -0800

    {"type":"TI", "runs":"2"}

--- createBall.java
+++ createBall.java
@@ -10,7 +10,6 @@ public class createBall extends GraphicsProgram{
 	public createBall(double r){
 		newCircle = new GOval(r, r);
 		newCircle.setFilled(true);
-		newCircle.setFillColor(Color.BLACK);
 		add(newCircle,0,0);
 	}
 
