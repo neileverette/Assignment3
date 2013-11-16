5a3bd9f9ad95e700ac4c10f90432271aa96adff5
S <s>
2013-11-16 10:31:30 -0800

    {"type":"TI", "runs":"0"}

--- DrawFace.java
+++ DrawFace.java
@@ -11,7 +11,7 @@ public class DrawFace extends GraphicsProgram {
  private static final int FACE_HEIGHT = 200; 
  
  public void run() { 
-	 GFace face = new GFace(FACE_WIDTH, FACE_HEIGHT); 
-	 add(face, (getWidth() - FACE_WIDTH) / 2, (getHeight() - FACE_HEIGHT) / 2); 
+	 GFace face = new GFace(30, 30); 
+	 add(face, (100, 200)); 
  } 
 }
