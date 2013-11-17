9032dbcf379023009d0daf839f5b3409fd95860d
S <s>
2013-11-17 00:13:35 -0800

    {"type":"TI", "runs":"0"}

--- dragCircle.java
+++ dragCircle.java
@@ -1,4 +1,29 @@
+import java.awt.Color;
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.event.*;
 
-public class dragCircle {
+public class dragCircle extends GraphicsProgram{
+	
+	// Instance variables
+	
+	
+	public void init(){
+		createCircles();
+		trackMouse();
+		
+		addMouseListeners();
+	}
+	
+	private void createCircles(){
+		
+	}
+	
+	private void trackMouse(){
+		GLabel label = new GLabel("no mouse detected");
+		label.setFont("Helvetica - 18");
+		add(label);
+		
+	}
 
 }
