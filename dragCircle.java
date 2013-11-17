f6093d65e8794e061f955185a7019a2e070e6df9
S <s>
2013-11-17 00:20:24 -0800

    {"type":"TI", "runs":"0"}

--- dragCircle.java
+++ dragCircle.java
@@ -20,7 +20,7 @@ public class dragCircle extends GraphicsProgram{
 	}
 	
 	private void trackMouse(){
-		GLabel label = new GLabel("no mouse detected");
+		label = new GLabel("no mouse detected");
 		label.setFont("Helvetica-20"); 
 		add(label, getWidth()/2 - label.getWidth()/2, label.getHeight());
 	}
