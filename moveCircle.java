06d349f04bea82272c378e74a0860fb9ee9a75cd
S <s>
2013-11-16 23:42:01 -0800

    {"type":"TI", "runs":"0"}

--- moveCircle.java
+++ moveCircle.java
@@ -17,10 +17,12 @@ public class moveCircle extends GraphicsProgram{
 	private GOval mouseBall;
 	
 	
-	public void init(){
+	public void run(){
 		
-		// Creates a circle from the external class with no parameters
+		// Creates a circle a method in the main class that takes no parameters
 		createCircle();
+		
+		// Creates a circle a method in the main class that takes parameters
 		makeBall(X_DIMIN,100,30);
 		
 		// Creates a circle from the external class that takes one parameter
@@ -31,25 +33,30 @@ public class moveCircle extends GraphicsProgram{
 		createBall ball2 = new createBall();
 		add(ball2, X_DIMIN, 300);	
 		
+		// Creates a circle from external class with no instance variable		
 		ball3 = new createBall(70);
 		add(ball3, 200, 200);
 		
+		// Creates a circle from external class with all parameters
 		ball4 = new createBall(300,200,30,Color.ORANGE);
 		add(ball4);
 		
+		// Creates a circle within main run method
 		mouseBall = new GOval(100,100,30,30);
 		mouseBall.setFilled(true);
 		mouseBall.setColor(Color.BLACK);
 		add(mouseBall);
 		
+		// Label to track the mouse coordinates
 		label = new GLabel(""); 
 		label.setFont("Helvetica-20"); 
 		add(label, getWidth()/2 - label.getWidth(), label.getHeight()); 
 		
+		// Listener that tracks the mouse
 		addMouseListeners();
 	}
 	
-	// Method that uses an instance variable
+	// Creates a circle within the main class
 	private void createCircle(){
 		ball = new GOval (X_DIMIN, Y_DIMIN, DIAM, DIAM);
 		ball.setFilled(true);
@@ -57,7 +64,7 @@ public class moveCircle extends GraphicsProgram{
 		add(ball);
 	}
 	
-	// Method that returns a GObject
+	// Creates a circle from method in the main class that takes parameters
 	public GOval makeBall(double x, double y, double r){
 		GOval circle = new GOval(x,y,r,r);
 		circle.setFilled(true);
