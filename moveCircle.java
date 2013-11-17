1570833b677f901c51bbb8f1784ae12e0331e7ad
S <s>
2013-11-16 22:58:16 -0800

    {"type":"TI", "runs":"1"}

--- moveCircle.java
+++ moveCircle.java
@@ -60,4 +60,9 @@ public class moveCircle extends GraphicsProgram{
 	public void mouseMoved(MouseEvent e){
 		mouseBall.setLocation(e.getX()-15, e.getY()-15);
 	}
+	
+	public void mousePressed(MouseEvent e){
+		last = new GPoint(e.getPoint());
+		gobj = getElementAt(last);
+	}
 }
