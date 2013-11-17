bcbc5c8129345e9381da99a0c1b089345d608811
S <s>
2013-11-16 23:51:33 -0800

    {"type":"TI", "runs":"1"}

--- createBall.java
+++ createBall.java
@@ -12,6 +12,7 @@ public class createBall extends GCompound {
 		
 		GLabel label = new GLabel("A");
 		label.setFont("Helvetica-20");
+		label.setColor(Color.WHITE)
 	
 		add(newCircle);
 		add(label, newCircle.getWidth()/2 - label.getWidth()/2, newCircle.getHeight()/2 + label.getHeight()/3);
