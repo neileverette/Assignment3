59d02ef85c6ca034cc639a050145779a1b44ce85
S <s>
2013-11-17 08:53:24 -0800

    {"type":"TI", "runs":"0"}

--- makeBall.java
+++ makeBall.java
@@ -1,4 +1,19 @@
+import java.awt.Color;
+import acm.graphics.*;
 
-public class makeBall {
+public class makeBall extends GCompound{
+	
+	private GOval newCircle;
+	
+	public makeBall (String name, double r){
+		newCircle = new GOval(r, r);
+		
+		GLabel label = new GLabel("name");
+		label.setFont("Helvetica-20");
+		label.setColor(Color.WHITE);
+	
+		add(newCircle);
+		add(label, newCircle.getWidth()/2 - label.getWidth()/2, newCircle.getHeight()/2 + label.getHeight()/3);
+	}
 
 }
