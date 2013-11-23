2181889f863ed258ad10802b4d3b4ff97587235a
S <s>
2013-11-23 13:27:17 -0800

    {"type":"TI", "runs":"0"}

--- upperCase.java
+++ upperCase.java
@@ -3,12 +3,17 @@ import acm.program.*;
 public class upperCase extends ConsoleProgram{
 
 	public void run(){
-	
+
 		askWord();
+//		convertUpperCase(String word);
 	}
 	
 	private String askWord(){
 		String word = readLine("Enter a word: ");
 		return word;
+	}
+	
+	private String convertUpperCase(String word){
+		
 	}
 }
