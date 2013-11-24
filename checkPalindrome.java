1b9c155409c97998b515050e89309da1d122a015
S <s>
2013-11-24 11:41:23 -0800

    {"type":"TI", "runs":"1"}

--- checkPalindrome.java
+++ checkPalindrome.java
@@ -19,9 +19,11 @@ public class checkPalindrome extends ConsoleProgram{
 		
 		for (int i = 0; i < keyword.length()/2; i++){
 			if (keyword.charAt(i) != keyword.length() - (i +1)){
+				println("Not a palindrone");
 				return false;
 			}
 		}
+		println("Is a palindrone");
 		return true;
 	}
 	
