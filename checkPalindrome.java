d9b73fc04011017dffd084dee93712f3a25c6648
S <s>
2013-11-24 10:06:29 -0800

    {"type":"TI", "runs":"0"}

--- checkPalindrome.java
+++ checkPalindrome.java
@@ -1,15 +1,20 @@
 import acm.program.*;
 
-public class checkPalindrome extends consoleProgram{
+public class checkPalindrome extends ConsoleProgram{
 	
 	private String keyword;
 	
 	public void run(){
 		askKeyword();
+		checkWord(keyword);
 	}
 	
 	private String askKeyword(){
 		keyword = readLine("Enter your word : ");
 		return keyword;
+	}
+	
+	private void checkWord(String keyword){
+		
 	}
 }
