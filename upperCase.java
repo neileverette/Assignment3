358157f8c67c65ce1c8a3f75c985e9a0e80cf80b
S <s>
2013-11-24 08:17:23 -0800

    {"type":"TI", "runs":"1"}

--- upperCase.java
+++ upperCase.java
@@ -29,12 +29,13 @@ public class upperCase extends ConsoleProgram{
 	
 	private void reverseLetters(String keyword){
 		String result = "";
+		char ch = keyword.charAt(3);
+		result = Character.toString(ch);
 		
-		for(int i = keyword.length(); i > 0; i--){
-			char ch = keyword.charAt(i);
-			result += Character.toString(ch);
-			
-		}
+//		for(int i = keyword.length(); i > 0; i--){
+//			char ch = keyword.charAt(i);
+//			result += Character.toString(ch);			
+//		}
 		println(result);
 	}
 
