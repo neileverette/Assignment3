8a55cdc45dd021813808f04266bbcf9a6a8a7701
S <s>
2013-11-24 08:50:41 -0800

    {"type":"TI", "runs":"0"}

--- checkPalindrome.java
+++ checkPalindrome.java
@@ -1,4 +1,14 @@
+import acm.program.*;
 
 public class checkPalindrome {
-
+	
+	private String keyword;
+	
+	public void run(){
+		askKeyword();
+	}
+	
+	private void askKeyword(){
+		keyword = readLn("What is your word? ");
+	}
 }
