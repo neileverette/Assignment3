f69b9dbf0679f77f1ef7f82a3d7a15770e443e67
S <s>
2013-11-24 18:04:48 -0800

    {"type":"TI", "runs":"0"}

--- countUppercase.java
+++ countUppercase.java
@@ -1,4 +1,16 @@
+import acm.program.*;
 
-public class countUppercase {
+public class countUppercase extends ConsoleProgram{
+	
+	private String keyword;
+	
+	public void run(){
+		askWord();
+	}
+	
+	public String askWord(){
+		keyword = readLine("Enter your word: ");
+		return keyword;
+	}
 
 }
