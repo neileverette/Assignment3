cc48d50bf179f97e1de96b9d0b6226c74a7030d2
S <s>
2013-11-29 16:46:58 -0800

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -137,7 +137,7 @@ public class Breakout extends GraphicsProgram {
 	        // Right Arrow key   
 	    	//paddle.setLocation(paddle.getX() + PADDLE_SPEED, paddle.getY());
 	    	dx = dx + 1;
-	    	paddle.setLocation(dx, dy);
+	    	paddle.move(dx, dy);
 	    	
 	    } else if (e.getKeyCode() == KeyEvent.VK_LEFT ) {
 	        //Left arrow key
