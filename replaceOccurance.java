1acf6f47a2420cd471dc101dc4ce7b176ae6e2fd
S <s>
2013-11-30 08:42:19 -0800

    {"type":"TI", "runs":"0"}

--- replaceOccurance.java
+++ replaceOccurance.java
@@ -1,4 +1,15 @@
+import acm.program.*;
 
-public class replaceOccurance {
+public class replaceOccurance extends ConsoleProgram{
+	
+	private String replaceFirstOccurance(String str, String orig, String repl){
+		int index = str.indexOf(orig);
+		
+		if(index != -1){
+			str = str.substring(0, index)
+		}
+		
+		
+	}return str
 
 }
