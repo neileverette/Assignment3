531aa05ce10cd2f02439c3b9ba6a235c0edb32cc
S <s>
2013-12-01 16:26:16 -0800

    {"type":"TI", "runs":"0"}

--- cipher.java
+++ cipher.java
@@ -2,22 +2,13 @@ import acm.program.*;
 
 public class cipher extends ConsoleProgram{
 	
-	private String encrypt(String str, int key){
-		
-		String result = "";
-		
-		for(int i=0; i < str.length(); i++){
-			
-		}
-	
-		return str;
-	}
+
 	
 	public void run(){
 		println("This program is a Ceasar Cipher program");
 		int key = readInt("Enter your key:");
-		String plainText = readLine("Enter your message: ");
-		String cipherText = encrypt(plainText, key);
-		println("Encrypted message is:" + cipherText);
+		String plaintext = readLine("Enter your message: ");
+		String ciphertext = encrypt(plaintext, key);
+		println("Encrypted message is:" + ciphertext);
 	}
 }
