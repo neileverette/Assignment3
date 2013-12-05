16233803d421ba30e31228b692dbee3a817e7c1d
S <s>
2013-12-05 08:51:16 -0800

    {"type":"TI", "runs":"0"}

--- cipher.java
+++ cipher.java
@@ -12,7 +12,7 @@ public class cipher extends ConsoleProgram{
 		
 		for(int i = 0; i < str.length(); i++){
 			char ch = str.charAt(i) ;
-			result += ch;
+			result += encryptChar(ch, key);
 		}
 		
 		return result;
