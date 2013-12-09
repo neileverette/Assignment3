963eef6684c0ffa977f76c62028e627d8699c02d
S <s>
2013-12-09 14:43:34 -0800

    {"type":"TI", "runs":"1"}

--- dragCircle.java
+++ dragCircle.java
@@ -91,7 +91,7 @@ public class dragCircle extends GraphicsProgram{
 	}
 	
 	public void mouseReleased(MouseEvent e){
-		//gobj.setColor(lastColor);
+		gobj.setColor(lastColor);
 		gobj.setVisible(true);
 	}
 	
