8e664532df9892d58de8fb998a927b90d1f73269
S <s>
2013-12-10 19:05:51 -0800

    {"type":"TI", "runs":"0"}

--- CalcMins.java
+++ CalcMins.java
@@ -4,6 +4,7 @@ public class CalcMins extends ConsoleProgram{
 	
 	public void run(){
 		println("Brooke's Minute Calculator");
+		
 		//int days = readInt("How many days would you like to calculate? ");
 	}
 	
