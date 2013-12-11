64d092eab583440330e36463d54efc300da2ad42
S <s>
2013-12-10 19:13:33 -0800

    {"type":"TI", "runs":"1"}

--- CalcMins.java
+++ CalcMins.java
@@ -8,8 +8,12 @@ public class CalcMins extends ConsoleProgram{
 		int days = readInt("How many days would you like to calculate? ");
 		
 		int minutes = days * 24 * 60;
+		int seconds = days * 24 * 60 * 60;
 		
 		println("There are " + minutes + (" minutes in ") + days + (" days"));
+		println("There are " + seconds + (" seconds in ") + days + ("days"));
+		
+		//   There are ___ seconds in ____ days
 	}
 	
 }
