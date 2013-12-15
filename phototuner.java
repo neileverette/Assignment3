64dcc3dab8273b44fe50c3aac855f35671ff5fc5
S <s>
2013-12-15 07:43:52 -0800

    {"type":"TI", "runs":"0"}

--- phototuner.java
+++ phototuner.java
@@ -1,4 +1,19 @@
 
-public class phototuner {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+import java.awt.Color;
+
+public class phototuner extends GraphicsProgram{
+	
+	// 
+	private static final int pY = 100;
+	private static final int window_x = 200;
+	private static final int window_y = 200;
+	
+	public void run(){
+		
+	}
+	
 
 }
