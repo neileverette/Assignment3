2b0fe483b77f26f8d4a71affac5ec3a7049615a2
S <s>
2013-12-15 08:43:18 -0800

    {"type":"TI", "runs":"1"}

--- phototuner.java
+++ phototuner.java
@@ -26,7 +26,7 @@ public class phototuner extends GraphicsProgram{
 		GLabel header = new GLabel(MESSAGE);
 		header.setFont("Roboto-40");
 		double x_pos = getWidth()/2 - header.getWidth()/2;
-		double y_pos = getHeight()/2 - header.getHeight()/4;
+		double y_pos = getHeight()/4 - header.getHeight()/2;
 		 
 		add (header, x_pos, y_pos);
 	}
