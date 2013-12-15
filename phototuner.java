7448dbed4dfd23e9ba72b6e3c77b7a374a00240b
S <s>
2013-12-15 08:35:08 -0800

    {"type":"TI", "runs":"0"}

--- phototuner.java
+++ phototuner.java
@@ -9,10 +9,12 @@ import java.awt.Color;
 public class phototuner extends GraphicsProgram{
 	
 	// 
-	private static final int pY = 100;
-	private static final int window_x = 200;
-	private static final int window_y = 200;
-	private static final String message = "Photo Tuner";
+	private static final int P_Y = 100;
+	private static final int WINDOW_X = 200;
+	private static final int WINDOW_y = 200;
+	private static final String MESSAGE = "Photo Tuner";
+	private static final int APP_WIDTH = 200;
+	private static final int APP_HEIGHT = 600;
 	
 	public void run(){
 		createLabel();
@@ -20,12 +22,10 @@ public class phototuner extends GraphicsProgram{
 	}
 	
 	private void createLabel(){
-		GLabel header = new GLabel(message);
+		GLabel header = new GLabel(MESSAGE);
 		double x_pos = getWidth()/2 - header.getWidth()/2;
 		double y_pos = getHeight()/2 - header.getHeight()/2;
 		add (header, x_pos, y_pos);
 	}
-	
-	private void setCanvas(){
-	}
+
 }
