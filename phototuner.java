baa43105efac7621f9d6efe848612f2a620ddb78
S <s>
2013-12-15 14:28:35 -0800

    {"type":"TI", "runs":"0"}

--- phototuner.java
+++ phototuner.java
@@ -90,7 +90,7 @@ public class phototuner extends GraphicsProgram{
 	// This method controls the playhead of the image histogram
  	public void mouseMoved(MouseEvent e) { 		
  		//paddle.setLocation(e.getX()-PADDLE_WIDTH/2, paddle.getY());
- //		playhead.setLocation(e.getX(), LINE_BOTTOM-50,e.getX(), LINE_BOTTOM );
+ 		playhead.setLocation(e.getX(), ((Double)LINE_BOTTOM-50),e.getX(), ((Double(LINE_BOTTOM));
  		
 	 }
 	
