c087112d40b74908f29d0ed8bb32bb2089f87295
S <s>
2013-12-15 14:47:15 -0800

    {"type":"TI", "runs":"1"}

--- phototuner.java
+++ phototuner.java
@@ -31,7 +31,7 @@ public class phototuner extends GraphicsProgram{
 	private static final int LINES = 35;
 	
 	//  Sets height of playhead
-	private static final int PLAYHEAD_HEIGHT = LINES + 5;
+	private static final int PLAYHEAD_HEIGHT = LINES + 3;
 	
 /**Instance Variables*/
 	
@@ -65,7 +65,7 @@ public class phototuner extends GraphicsProgram{
 	
 	private void drawPlayhead(){
 		playhead_X = (getWidth() - PHOTO_DAYS)/2;
-		playhead = new GRect(playhead_X, LINE_BOTTOM, PLAYHEAD_WIDTH, PLAYHEAD_HEIGHT);
+		playhead = new GRect(playhead_X, LINE_BOTTOM + PLAYHEAD_HEIGHT , PLAYHEAD_WIDTH, PLAYHEAD_HEIGHT);
 		playhead.setFilled(true);
 		playhead.setColor(Color.red);
 		add(playhead);
