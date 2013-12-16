c8568675d532b930f255399bb7aa3ec96b134a08
S <s>
2013-12-15 17:43:04 -0800

    {"type":"TI", "runs":"0"}

--- phototuner.java
+++ phototuner.java
@@ -89,7 +89,7 @@ public class phototuner extends GraphicsProgram{
 	}
 
 	private void createLabels(){
-		GLabel present = new GLabel("Most Recent", (getWidth() - PHOTO_DAYS)/2, LINE_BOTTOM+PLAYHEAD_HEIGHT+present.getHeight()+10);
+		GLabel present = new GLabel("Most Recent", (getWidth() - PHOTO_DAYS)/2, LINE_BOTTOM+PLAYHEAD_HEIGHT + this.getHeight()+10);
 		add(present);
 	}
 	
