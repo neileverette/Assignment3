b290dcd6c7de3699af6b27bf7c5590397f25280d
S <s>
2013-12-15 18:39:21 -0800

    {"type":"TI", "runs":"1"}

--- phototuner.java
+++ phototuner.java
@@ -97,7 +97,7 @@ public class phototuner extends GraphicsProgram{
 		present = new GLabel("Most Recent", (getWidth() - PHOTO_DAYS)/2, LINE_BOTTOM - PLAYHEAD_HEIGHT - LABEL_MARGIN);
 		present.setFont("Helvetica-10");
 		
-		oldest = new GLabel("Oldest", (getWidth() - PHOTO_DAYS)/2+PHOTO_DAYS-oldest.getWidth()/2, LINE_BOTTOM - PLAYHEAD_HEIGHT - LABEL_MARGIN);
+		oldest = new GLabel("Oldest", (getWidth() - PHOTO_DAYS)/2+PHOTO_DAYS-oldest.getWidth(), LINE_BOTTOM - PLAYHEAD_HEIGHT - LABEL_MARGIN);
 		oldest.setFont("Helvetica-10");
 		
 		add(present);
