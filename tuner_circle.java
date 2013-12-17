3fd0fd658e0ee67957c103650c76ea40c41293ad
S <s>
2013-12-17 02:46:55 -0800

    {"type":"TI", "runs":"0"}

--- tuner_circle.java
+++ tuner_circle.java
@@ -1,4 +1,39 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+import java.awt.event.*;
+import java.awt.Canvas;
+import java.awt.Color;
+
 
 public class tuner_circle {
+	
+	
+	// Sets the number of "days" of the photos
+	private static final int PHOTO_DAYS = 700;
+	
+	//  Sets width of playhead
+	private static final int PLAYHEAD_WIDTH = 2;
+	
+	//  Sets height of histogram lines
+	private static final int LINES = 25;
+	
+	private static final int CIRCLE_RADIUS = 100;
+	
+	
+	public GOval circle;
+	
+	
+	public void run(){
+		drawCircle();
+	}
+
+	public void drawCircle(){
+		circle = new GOval(200,200,CIRCLE_RADIUS*2,CIRCLE_RADIUS*2);
+		add(circle);
+		
+		
+	}
+	
 
 }
