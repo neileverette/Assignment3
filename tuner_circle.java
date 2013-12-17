c73137a858c25eeb6d39ebf8e8febd9aaf01b896
S <s>
2013-12-17 03:27:43 -0800

    {"type":"TI", "runs":"0"}

--- tuner_circle.java
+++ tuner_circle.java
@@ -6,7 +6,7 @@ import java.awt.Canvas;
 import java.awt.Color;
 
 
-public class tuner_circle {
+public class tuner_circle extends GraphicsProgram{
 	
 	
 	// Sets the number of "days" of the photos
