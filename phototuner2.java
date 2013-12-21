0a6d83883f2a557b23e508ec869460eed17bf162
S <s>
2013-12-21 12:44:23 -0800

    {"type":"TI", "runs":"0"}

--- phototuner2.java
+++ phototuner2.java
@@ -1,4 +1,18 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+import java.awt.event.*;
+import java.awt.Canvas;
+import java.awt.Color;
 
-public class phototuner2 {
+public class phototuner2 extends GraphicsProgram{
+	
+	public static void main(String[] args) {
+		new phototuner2().start(args);
+	}
+	
+	public void run(){
+		
+	}
 
 }
