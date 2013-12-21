7e0dc373d3ba6071a753a67d35e89190c4eba965
S <s>
2013-12-21 11:41:45 -0800

    {"type":"TI", "runs":"0"}

--- scrubber.java
+++ scrubber.java
@@ -1,4 +1,13 @@
+import acm.graphics.*;
 
-public class scrubber {
+public class scrubber extends GCompound{
 
+	public GRect ridge;
+	
+	public drawRidge(double x, double y, double ridgeWidth, double ridgeHeight){
+		ridge = new GRect(x,y, ridgeWidth, ridgeHeight);
+		
+		add(ridge);
+	}
+	
 }
