776dc7b3334bef369ab2b00a5eb8a65d4761b8a6
S <s>
2013-12-21 12:40:45 -0800

    {"type":"TI", "runs":"0"}

--- scrubber.java
+++ scrubber.java
@@ -1,12 +1,24 @@
+import java.awt.Color;
+
 import acm.graphics.*;
 
 public class scrubber extends GCompound{
 
 	public GRect ridge;
 	
-	public void drawRidge(double x, double y, double ridgeWidth, double ridgeHeight){
-		ridge = new GRect(x,y, ridgeWidth, ridgeHeight);
+	public void drawRidge(double ridgeWidth, double ridgeHeight,double x, double y){
+		ridge = new GRect(ridgeWidth, ridgeHeight, x, y);
+		ridge.setFilled(true);
+		ridge.setColor(Color.LIGHT_GRAY);
 		add(ridge);
 	}
 	
+	
+	public void drawRidges(double ridgeWidth, double ridgeHeight,double x, double y){
+		
+		for(int i=0; i<getHeight(); i++){
+			drawRidge(ridgeWidth, ridgeHeight, x, y*i);
+		}
+		
+	}
 }
