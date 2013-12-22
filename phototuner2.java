a3dcdfd9d0d7e7a40f0d9112618d096d963daac2
S <s>
2013-12-21 19:32:25 -0800

    {"type":"TI", "runs":"2"}

--- phototuner2.java
+++ phototuner2.java
@@ -11,33 +11,9 @@ public class phototuner2 extends GraphicsProgram{
 		new phototuner2().start(args);
 	}
 	
-	// Static variables
-	private static final int RIDGEHEIGHT = 2;
-	private static final int RIDGESSPACING = 2;
-	private static final double RIDGESIZE = .9;
-	
-	// Private instance variables
-	private scrubber scrubber;
-	private GLine ridge;
-	private GRect ridge2;
-	
-	
 	public void run(){
 		setSize(480, 800);
-		createLine(RIDGESIZE*getWidth(), 50, getWidth(), 50);
-		createBox();
 	}
 
-	private void createLine(double x0, double y0, double x1, double y1){
-		ridge = new GLine(x0, y0, x1, y1);
-		add(ridge);
-	}
-	
-	private void createBox(){
-		ridge2 = new GRect(100, 2, 300, 3);
-		ridge2.setFilled(true);
-		ridge2.setFillColor(Color.LIGHT_GRAY);
-		add(ridge2);
-		}
 		
 	}
