6501386de9d7afccf7c39b4d7fae23ae261e5cf7
S <s>
2013-12-22 15:07:39 -0800

    {"type":"TI", "runs":"0"}

--- phototuner2.java
+++ phototuner2.java
@@ -36,7 +36,7 @@ public class phototuner2 extends GraphicsProgram{
 	}
 	
 	public void createScrubber(){
-		myscrubber = new scrubber();
+		myscrubber = new scrubber(HEIGHT, WIDTH);
 		myscrubber.setLocation(50, 100);
 		add(myscrubber);
 	}
