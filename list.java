0162bd21c2f019ceb5652a279aac14ece3f4e742
S <s>
2013-12-22 20:08:50 -0800

    {"type":"TI", "runs":"0"}

--- list.java
+++ list.java
@@ -7,7 +7,7 @@ public class list extends GCompound{
 	// Instance variables
 	private GRect newRidge;
 	
-	public list(int height, int width, Color color, int listNumber){
+	public list(int height, int width, int gap, Color color, int listNumber){
 		
 		for (int i=0; i<listNumber; i++){
 			newRidge = new GRect(0, i*10, width, height);
