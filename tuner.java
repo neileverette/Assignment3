9018f92307cad6913ba68b66b195c0051a2b3854
S <s>
2013-12-22 20:12:53 -0800

    {"type":"TI", "runs":"0"}

--- tuner.java
+++ tuner.java
@@ -23,7 +23,7 @@ public class tuner extends GraphicsProgram{
 	
 	// Instance Variables
 	private list myList;
-	private int listNumber = 3;
+	private int listNumber = 20;
 	
 	public void run(){
 		setSize(WINDOWWIDTH, WINDOWHEIGHT);
