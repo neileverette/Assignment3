7707d8b0380bfe8899578abab66200e8748d459c
S <s>
2013-12-22 20:11:11 -0800

    {"type":"TI", "runs":"1"}

--- tuner.java
+++ tuner.java
@@ -32,7 +32,7 @@ public class tuner extends GraphicsProgram{
 	
 	public void createList(){
 		myList = new list(RIDGEHEIGHT, RIDGEWIDTH, RIDGEGAP, COLOR, listNumber);
-		add(myList, getWidth()/2, 100);
+		add(myList, getWidth()/2-5, 100);
 	}
 		
 }
