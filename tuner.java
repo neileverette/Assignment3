d4e409d8517eee9271bd45a090e893619ac266b9
S <s>
2013-12-22 19:07:48 -0800

    {"type":"TI", "runs":"0"}

--- tuner.java
+++ tuner.java
@@ -17,7 +17,7 @@ public class tuner extends GraphicsProgram{
 	private static final int WINDOWHEIGHT = 800;
 	private static final int RIDGEHEIGHT = 5;
 	private static final int RIDGEGAP = 4;
-	private static fianl int RIDGEWIDTH = 300;
+	private static final int RIDGEWIDTH = 300;
 	
 	
 	// Instance Variables
