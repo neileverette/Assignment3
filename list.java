982415f67b9482107537ff74cab02ef30d5a1377
S <s>
2013-12-22 20:05:55 -0800

    {"type":"TI", "runs":"0"}

--- list.java
+++ list.java
@@ -7,12 +7,15 @@ public class list extends GCompound{
 	// Instance variables
 	private GRect newRidge;
 	
-	
-	public list(int height, int width, Color color){
-		newRidge = new GRect(width, height);
-		newRidge.setFilled(true);
-		newRidge.setFillColor(color);
-		add(newRidge);
+	public list(int height, int width, Color color, int listNumber){
+		
+		for (int i=0; i<listNumber; i++){
+			newRidge = new GRect(width, height);
+			newRidge.setFilled(true);
+			newRidge.setFillColor(color);
+			add(newRidge);
+		}
+		
 	}
 	
 }
