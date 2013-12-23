5ac023c57bac4d0a1017e82fd26ce20c9ed53194
S <s>
2013-12-22 18:46:18 -0800

    {"type":"TI", "runs":"0"}

--- list.java
+++ list.java
@@ -1,4 +1,16 @@
+import java.awt.Color;
 
-public class list {
+import acm.graphics.*;
+
+public class list extends GCompound{
+	
+	// Instance variables
+	private GRect newRidge;
+	
+	public void list(){
+		newRidge = new GRect(300, 100);
+		newRidge.setFilled(true);
+		newRidge.setFillColor(Color.black);
+	}
 
 }
