8b3ce28eba85ee3ce66b94b2b78136c25663cff6
S <s>
2013-12-24 16:04:18 -0800

    {"type":"TI", "runs":"0"}

--- phototuner.java
+++ phototuner.java
@@ -62,7 +62,7 @@ public class phototuner extends GraphicsProgram{
 		createBackground();
 		createLabels(); // Create the top label
 		drawLines(); // Draw the graph
-		baseline((getWidth() - PHOTO_DAYS)/2, LINE_BOTTOM, PHOTO_DAYS+(getWidth() - PHOTO_DAYS)/2), LINE_BOTTOM);
+		baseline((getWidth() - PHOTO_DAYS)/2, LINE_BOTTOM, PHOTO_DAYS+(getWidth() - PHOTO_DAYS)/2, LINE_BOTTOM);
 		drawPlayhead(); // Draw the playhead
 	}
 	/** THIS SIMULATES THE BACKGROUND SWAPPING WITH PHOTOS*/
